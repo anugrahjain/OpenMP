6b1ff028de013b9bda126bacd912d118458c4815
anugrahjain <anugrahjain4u@gmail.com>
2025-04-10 13:38:59 +0530

    Add files via upload

---
+++ 4_race_condition_solution.c
@@ -0,0 +1,14 @@
+#include <omp.h>
+#include <stdio.h>
+int main( int *argc, char *argv[] )
+{
+/* Declare numt and tid outside parallel block */
+int numt, tid;
+#pragma omp parallel private(tid) shared(numt)
+{
+numt = omp_get_num_threads();
+tid = omp_get_thread_num();
+for(int i=0;i<10000000;i++);
+printf( "Hello World from thread %d of %d.\n", tid, numt );
+}
+}
---
+++ 7_interations-assignment.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<omp.h>
+#define ARR_SIZE 10
+int main( int *arg, char *argv[] )
+{
+int j, tid ;
+int a[ARR_SIZE] ;
+#pragma omp parallel private( tid)
+{
+tid = omp_get_thread_num() ;
+
+// schedule(static, 2) or schedule(dynamic, 2) 
+
+#pragma omp for 
+for( j = 0 ; j < ARR_SIZE ; j++ )
+{
+a[j] = 1 ;
+printf( "Thread %d, iteration %d\n", tid, j ) ;
+}
+}
+return 0;
+}
