3fa2d52d403e921366b69e378b9a3073e2adc30b
anugrahjain <anugrahjain4u@gmail.com>
2025-07-20 20:51:58 +0530

    Update 5_threadprivate.c

--- 5_threadprivate.c
+++ 5_threadprivate.c
@@ -1,10 +1,14 @@
 #include <omp.h>
 #include <stdio.h>
+
 int tid ;
+
 #pragma omp threadprivate( tid )
+
 void main()
 {
 int numt ;
+
 #pragma omp parallel default( shared )
 {
 tid = omp_get_thread_num() ;
@@ -14,6 +18,8 @@ for(int j = 0 ; j < 10000000 ; j++ ) ;
 numt = omp_get_num_threads() ;
 }
 }
+
 #pragma omp parallel default( shared )
 printf( "Hello World from thread %d of %d.\n", tid, numt ) ;
+
 }
